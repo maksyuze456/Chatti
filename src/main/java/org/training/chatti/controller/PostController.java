621defbe80c988148deb906395fbba6f293faa11
Maksym <maksym.045z@gmail.com>
2025-08-25 13:03:50 +0200

    Unused import

--- src/main/java/org/training/chatti/controller/PostController.java
+++ src/main/java/org/training/chatti/controller/PostController.java
@@ -1,6 +1,5 @@
 package org.training.chatti.controller;
 
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
